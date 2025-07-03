ba27a842dc8f98cef50f38faf81de73a99ee4552
flockwood <flockwood@live.com>
2025-07-03 15:33:30 -0400

    application factory includes jwt

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
-# Create bcrypt instance
+# Create extension instances
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -13,6 +15,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     
     # Initialize extensions
     bcrypt.init_app(app)
+    jwt.init_app(app)
     
     # Initialize API
     api = Api(
@@ -35,5 +38,9 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     from app.api.v1.reviews import api as reviews_ns
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
+    # Register auth namespace
+    from app.api.v1.auth import api as auth_ns
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     return app
