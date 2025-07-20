fc50ffd3e7efbac218f4d7b30486fc91bfd052ca
flockwood <flockwood@live.com>
2025-07-20 15:47:54 -0400

    added cors to requirements init and scrypt js update

--- part4/part3/app/__init__.py
+++ part4/part3/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 # Create extension instances
 bcrypt = Bcrypt()
